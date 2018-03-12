9c5d852716a9ffe11c1156caf24f37b9d8e997df
yiwei <yw349762@dal.ca>
2018-03-11 23:51:53 -0300

    change detail activity

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -6,10 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class DetailViewActivity extends Activity {
 
     private EditText nameField, emailField,businessNumberField,primaryBusinessField,addressField,provinceField;
-    Contact receivedPersonInfo;
+    private Contact receivedPersonInfo;
+    private DatabaseReference myRef;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,9 @@ public class DetailViewActivity extends Activity {
         addressField=(EditText) findViewById(R.id.addressDetail);
         provinceField=(EditText) findViewById(R.id.provinceDetail);
 
+        database=FirebaseDatabase.getInstance();
+        myRef=database.getReference();
+
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
             emailField.setText(receivedPersonInfo.email);
@@ -36,6 +47,21 @@ public class DetailViewActivity extends Activity {
 
     public void updateContact(View v){
         //TODO: Update contact funcionality
+        String uid=receivedPersonInfo.uid;
+        String newname=nameField.getText().toString();
+        String newemail=emailField.getText().toString();
+        int newbusinessNumber=Integer.parseInt(businessNumberField.getText().toString());
+        String newprimaryBusiness=primaryBusinessField.getText().toString();
+        String newaddress=addressField.getText().toString();
+        String newprovince=provinceField.getText().toString();
+
+        Contact newInfo=new Contact(uid,newname,newemail,newbusinessNumber,newprimaryBusiness,newaddress,newprovince);
+
+
+
+        myRef.child(receivedPersonInfo.uid).setValue(newInfo);
+
+
     }
 
     public void eraseContact(View v)
